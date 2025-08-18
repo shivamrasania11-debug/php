d5ebb8cf95b84853441b46229f7d94970cc92c73
shivamrasania11-debug <shivamrasania11@gmail.com>
2025-08-18 19:36:47 +0530

    Create 09-04_sum_of_factorial.php

---
+++ 09-04_sum_of_factorial.php
@@ -0,0 +1,37 @@
+<?php
+$res=" ";
+if(isset($_POST['submit']))
+{
+$n=$_POST['num1'];
+$fact=1;
+$sum=0;
+ for($i=1;$i<=$n;$i++)
+{
+	$fact=$fact*$i;
+	$sum=$sum+$fact;
+}
+	$res=$sum;
+}
+?>
+<html>
+<head>
+<title>factorial number</title>
+</head>
+<body>
+<form method="post">
+	<table border="1" align="center">
+	<tr>
+		<td>enter the number:</td>
+		<td><input type="text" name="num1"></td>
+	</tr>
+	<tr align="center">
+		<td colspan="2"><?php echo " " .$res; ?></td>
+	</tr>
+	<tr align="center">
+		<td colspan="2"><input type="submit" name="submit"></td>
+	</tr>
+	</table>
+</table>
+</form>
+</body>
+</html>
